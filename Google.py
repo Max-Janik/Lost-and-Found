3a388068d27ed48742cc95bd62e96b5a0e164d09
Max-Janik <35370323+Max-Janik@users.noreply.github.com>
2018-01-12 13:43:19 +0100

    Add files via upload

---
+++ Google.py
@@ -0,0 +1,204 @@
+﻿#!/usr/bin/env python
+
+#OPEN LOG FILE FOR DEBUGGING
+f = open(log.txt, w)
+
+#WRITE Log1
+file.write("0.0")
+
+#IMPORTS
+
+import mechanize
+import MySQLdb
+import sys
+from bs4 import BeautifulSoup
+
+#LISTS
+
+    #PUNCTUATION
+ptc = ["|", "<", ">", "!", "{", "[", "]", "}", "/", ",", ";", ".", ":", "-", "_", "#", "'", "+", "*", "~", "\"", "=", "&", "%", "$", "§", "?", "(", ")"]
+    #EXTRAORDINARY CHARACTERS
+uncommon = ["é", "ü", "ö", "ä", "ú", "ù", "á", "à"]
+    #REPLACEMENT
+replace = ["e", "ue", "oe", "ae"]
+    #IRRELEVANT CHARACTERS
+misc = ["\\xd", "\\xa", "\\x", "\\u"]
+    #HTML TAGS
+htmlTags = ["br", "b", "tr", "td"]
+    #NUMBERS
+numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+    #FILE-EXTENSIONS
+Ext = ["jpg", "jpeg", "gif", "png", " k "]
+    #ARGV - Input from Webpage
+ar = sys.argv
+    #FREQUENCY OF WORDS
+freq = []
+    #MOST COMMON WORDS
+data = [0,0,0,0,0]
+
+
+#BROWSER SETUP
+
+br = mechanize.Browser()
+br.set_handle_robots(False)
+br.set_handle_refresh(False)
+br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
+
+
+#DB SETUP
+
+db = MySQLdb.connect("rdbms.strato.de", "U3140417", "tP2GHz7WTSCE", "DB3140417")
+cur = db.cursor()
+
+#DEF's
+
+#LOOKING UP THE DESIGNATED IMAGE
+def ReverseImageLookup():
+
+    filename = ar[4]
+    
+    #GET SEARCH WEBSITE
+    #OPEN IMAGES.GOOGLE.COM
+    br.open( "http://images.google.com" )                                         
+    #SELECT FORM
+    br.select_form( 'f' )                                                         
+    #IMAGE URL
+    br.form[ 'q' ] = 'https://lost-and-found-app.de/pics/'+filename               
+    #SUBMIT FORM
+    br.submit()                                                                   
+    #READ RESPONSE
+    response = br.response().read()                                               
+    
+    
+    
+    #REPLACE LATIN CHARs
+    for x in range(0, len(uncommon)):
+        response = response.replace(uncommon[x], replace[x])
+    
+    #BEAUTIFUL SOUP SETUP
+    soup = BeautifulSoup(response, "html.parser")
+    soup_div = soup.findAll("div", {"id":"ires"})
+    soup_tr = soup_div[0].findAll("tr")
+
+
+    
+
+    #DECOMPOSE a-TAGS
+    for x in range(0, len(soup_tr)):
+        for div in soup_tr[x].findAll("a"): 
+            div.decompose()
+
+    #DECOMPOSE cite-TAGS
+    for x in range(0, len(soup_tr)):
+        for div in soup_tr[x].findAll("cite"): 
+            div.decompose()
+
+    
+    
+    #CONVERT TO STRING
+    string = str(soup_tr)
+
+    #MAKE ALL CHARACTERS LOWERCASE
+    string = string.lower()
+    
+    #DELETE "STYLE" HTML TAGs
+    while True:
+        try:
+            st = string[:string.index('style')]
+            rng = string[string.index('">')+1:]
+            string = st+rng
+        except ValueError:
+            break
+
+    #DELETE NUMBERS
+    for x in range(0, len(numbers)):
+        string = string.replace(numbers[x], "")
+
+    #DELETE HTML TAGs
+    for x in range(0, len(htmlTags)):
+        string = string.replace(htmlTags[x], "")
+
+    #DELETE PUNCTUATION
+    for x in range(0, len(ptc)):
+        string = string.replace(ptc[x], "")
+
+    #DELETE UNNESSECARY CHARs
+    for x in range(0, len(misc)):
+        string = string.replace(misc[x], "")
+
+    #DELETE FILE-EXTENSIONS
+    for x in range(0, len(Ext)):
+        string = string.replace(Ext[x], "")
+
+
+
+
+    #SPLIT STRING INTO LIST, COMPARE FREQUENCY
+
+    #SPLIT ARRAY BY WORDS
+    split = string.split()
+
+    #FILL FREQUENCY LIST
+    for x in range(0, len(split)):
+        freq.append(0)
+
+    #GET FREQUENCY
+    for x in range(0, len(split)):
+        for y in range(0, len(split)):
+            if split[x] == split[y]:
+                freq[x] = freq[x]+1
+    
+    
+    #ERASE REPEATING WORDS EXCEPT THE FIRST           
+    for x in range(0, len(split)):
+        for y in range(0, len(split)):
+            if x != y:
+                if split[x] == split[y]:
+                    freq[y] = -1
+                    split[y] = ""
+
+
+    #SORT BY FREQUENCY
+    c = 0
+    for x in range(0, len(data)):
+        for y in range(0, len(freq)):
+            if freq[y] > freq[c]:
+                data[x] = split[y]
+                print data[x]
+                c = y
+        print c
+        freq[c] = 0
+'''
+    
+    print split
+    print freq
+    print data
+
+'''
+return data
+    #END OF DEF
+
+
+
+#WRITING THE GATHERD DATA INTO THE DATABASEs
+
+def MySQL_Write(name, location, description, filename, keywords):
+    
+    cur.execute("INSERT INTO found(Name, Location, Description, Picture, ID, Keywords) Values('"+name+"', '"+location+"', '"+description+"', '"+filename+"', '', '"+keywords+"')")
+
+    #END OF DEF
+    
+#PROGRAMM
+f.write("1.0")    
+Keywords = ReverseImageLookup()
+f.write("1.5")
+MySQL_Write(ar[1], ar[2], ar[3], ar[4], Keywords)
+f.write("2.0")
+
+
+
+
+
+#END
+f.close()
+db.close()
---
+++ main.py
@@ -0,0 +1,209 @@
+#IMPORTS
+import pygame
+import os
+import socket
+
+class APP:
+
+    #DEF's
+
+
+    def __init__(self):
+
+        
+        pygame.init()
+        android = True
+        screen = pygame.display.set_mode((720/2, 1280/2))
+        screen_w, screen_h = screen.get_size()
+        button_h = screen_h*0.1
+        button_w = screen_w/3
+        header_h = 1.5 * button_h
+        #LOADING "NEARBY" BUTTON ICON
+        BNB_Icon = pygame.image.load("BNB-icon.png")
+        BNB_Icon = pygame.transform.scale(BNB_Icon, (int(button_w),int(button_h)))
+        HNB_Icon = pygame.image.load("HNB-icon.png")
+        HNB_Icon = pygame.transform.scale(HNB_Icon, (int(button_w),int(button_h)))
+        
+        #LOADING "NEW ITEM" BUTTON ICON
+        BN_Icon = pygame.image.load("BN-icon.png")
+        BN_Icon = pygame.transform.scale(BN_Icon, (int(button_w),int(button_h)))
+        HN_Icon = pygame.image.load("HN-icon.png")
+        HN_Icon = pygame.transform.scale(HN_Icon, (int(button_w),int(button_h)))
+
+        #LOADING "FOUND" BUTTON ICON
+        BF_Icon = pygame.image.load("BF-icon.png")
+        BF_Icon = pygame.transform.scale(BF_Icon, (int(button_w),int(button_h)))
+        HF_Icon = pygame.image.load("HF-icon.png")
+        HF_Icon = pygame.transform.scale(HF_Icon, (int(button_w),int(button_h)))
+
+        #LOADING HEADER
+        Header_Icon = pygame.image.load("Header-icon.png")
+        Header_Icon = pygame.transform.scale(Header_Icon, (int(screen_w),int(header_h)))
+
+        #LOADING FONTS
+        try:
+            pygame.font.init()
+            font = pygame.font.Font('DejaVuSans.ttf', 20)
+            print "Font loaded correctly!"
+        except Exception as fe:
+            print "Font Error!"
+            print fe
+
+        
+            
+        #SETTING UP THE SCREEN LAYOUT
+        screen.fill((255, 255, 255))    
+        button1 = screen.blit(BNB_Icon, [0, button_h*9])
+        button2 = screen.blit(BN_Icon, [button_w, button_h*9])
+        button3 = screen.blit(BF_Icon, [2*button_w, button_h*9])
+        Header = screen.blit(Header_Icon, [0,0])
+        pygame.display.flip()
+
+
+        def erase():
+            print "Erasing..."
+            screen.fill((255, 255, 255), (0,int(header_h),screen_w,int(0.75*screen_h)))
+            pygame.display.flip()
+
+        
+
+        #ACTUAL PROGRAMM
+        while True:
+
+            
+            #WAIT FOR EVENT TO OCCUR
+            event = pygame.event.wait()
+
+            
+            
+            # PYGAME QUIT
+            if event.type == pygame.QUIT:
+                break
+
+
+
+            #HITTING THE SCREEN
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+
+                #BUTTON 1 | ESTABLISHING CONNECTION
+                if button1.collidepoint(event.pos):
+                    
+                    button1 = screen.blit(HNB_Icon, [0, button_h*9])
+                    pygame.display.flip()
+                    #"Lost-and-Found-app.de/var/www/Server.py"
+                    try:
+                        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                        
+                        erase()
+                        textsurface = font.render("Socket Works!", False, (0, 0, 0))
+                        screen.blit(textsurface,(0,header_h))
+                        pygame.display.flip()
+
+                        global_ip = "Lost-and-Found-app.de"
+                        local_ip = "192.168.2.105"
+                        s.connect((global_ip,3389))
+                        print s.recv(1024)
+                        print s.recv(1024)
+                        print s.recv(1024)
+                        textsurface = font.render("Connected!", False, (0, 0, 0))
+                        screen.blit(textsurface,(0,1.25*header_h))
+                        pygame.display.flip()
+
+                        with open('empfangen.jpg','wb') as f:
+                            while True:
+                                recv = s.recv(1024)
+                                f.write(recv)
+                                s.send("1")
+                                textsurface = font.render("1 Datei von Server her", False, (0, 0, 0))
+                                screen.blit(textsurface,(0,1.75*header_h))
+                                pygame.display.flip()
+                                if len(recv) < 1024:
+                                    break
+
+                        textsurface = font.render("Recieved Data!", False, (0, 0, 0))
+                        screen.blit(textsurface,(0,1.5*header_h))
+                        pygame.display.flip()
+                        
+                        f.close()
+                        s.send("1")
+                        s.close()
+                        
+                        erase()
+                        prnt = pygame.image.load("empfangen.jpg")
+                        prnt = pygame.transform.scale(prnt,(screen_w,button_h*7.5))
+                        screen.blit(prnt,(0,header_h))
+                        pygame.display.flip()
+                        
+                    except Exception as ex:
+                        print "Connection Error"
+                        print str(ex)
+                        #erase()
+                        ex = str(ex)
+                        textsurface = font.render(ex, False, (0, 0, 0))
+                        screen.blit(textsurface,(0,2*header_h))
+                        pygame.display.flip()
+                        
+                #BUTTON 2 | NO FUNCTION; CHANGING SELF.COLOR      
+                elif button2.collidepoint(event.pos):
+                    
+                    button2 = screen.blit(HN_Icon, [button_w, button_h*9])
+                    pygame.display.flip()
+
+                #BUTTON 3 | NO FUNCTION; CHANGING SELF.COLOR  
+                elif button3.collidepoint(event.pos):
+                    
+                    button3 = screen.blit(HF_Icon, [2*button_w, button_h*9])
+                    pygame.display.flip()
+
+
+            try:
+
+                if android:
+                    # ANDROID BACK KEY
+                    if event.type == pygame.KEYDOWN and event.key == pygame.K_AC_BACK:
+                        break
+
+
+                    #LETTING GO OF BUTTONS
+                    elif event.type == pygame.MOUSEBUTTONUP:
+                        
+                        button1 = screen.blit(BNB_Icon, [0, button_h*9])
+                        button2 = screen.blit(BN_Icon, [button_w, button_h*9])
+                        button3 = screen.blit(BF_Icon, [2*button_w, button_h*9])
+                        pygame.display.flip()
+
+
+
+                    
+                    elif event.type == pygame.APP_WILLENTERBACKGROUND:
+
+                        print "sleeping now"
+                    elif event.type == pygame.APP_DIDENTERFOREGROUND:
+                        
+                        screen = pygame.display.set_mode((1280, 720))
+                        screen.fill((255, 255, 255))    
+                        button1 = screen.blit(BNB_Icon, [0, button_h*9])
+                        button2 = screen.blit(BN_Icon, [button_w, button_h*9])
+                        button3 = screen.blit(BF_Icon, [2*button_w, button_h*9])
+                        Header = screen.blit(Header_Icon, [0,0])
+                        pygame.display.flip()
+            except:
+                android == False
+
+        #QUITS THE APP WHEN BREAKING OUT THE WHILE LOOP
+        pygame.quit()
+
+
+
+
+#MAIN
+    
+if __name__ == "__main__":
+    APP()
+#END
+
+
+#NOTES:
+
+#python android.py build app release install --launch
+#font: Tw Cen MT Condensed, Bold
